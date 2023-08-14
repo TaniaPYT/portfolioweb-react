e94f9d57bfce950bf167dbf8a5a9a9b38ddec6a1
Tania-aa <128038741+TaniaPYT@users.noreply.github.com>
2023-08-14 12:56:24 +0800

    done v1

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,40 @@
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
   useEffect(() => {
     setTimeout (() => {
       setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+    emailjs
+      .sendForm (
+        'service_zr30y7t',
+        'template_xdiv6e9',
+        refForm.current,
+        'RoZ0zZRigMYReFNIb'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again.')
+        }
+      )
+  }
 
   return (
     <>
@@ -24,10 +48,11 @@ const Contact = () => {
             />
           </h1>
           <p>
-            I am interested in internship / freelance opportunities. However,don't hesitate to contact me using form below, if you have any other request / quesion or just want a chat.
+            I am interested in internship / freelance opportunities. <br />
+            However, don't hesitate to contact me using form below, if you have any other request / quesion or just want a chat.
           </p>
           <div className='contact-form'>
-            <form>
+            <form ref={refForm} onSubmit={sendEmail}>
               <ul>
                 <li className='half'>
                   <input
@@ -67,6 +92,21 @@ const Contact = () => {
             </form>
           </div>
         </div>
+      </div>
+      <div className='info-map'>
+        Yitong Peng
+        <br />
+        Guangzhou, Guangdong, China
+        <br />
+        <span>Taniapyt@outlook.com</span>
+      </div>
+      <div className='map-wrap'>
+        <MapContainer center={[23.128994, 113.253250]} zoom={13}>
+          <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+          <Marker position={[23.128994, 113.253250]}>
+            <Popup> Tania lives here !</Popup>
+          </Marker>
+        </MapContainer>
       </div>
       <Loader type='pacman' />
     </>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom'
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useEffect } from 'react';
+import MyPhoto from '../../assets/images/my-photo.jpg'
 import Logo from './Logo'
 import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
   const nameArray = ['a', 'n','i', 'a']
-  const jobArray = ['w','e','b','', 'd','e','v','l','o','p','e','r','.']
+  const jobArray = ['W','e','b','', 'D','e','v','l','o','p','e','r','.']
   
 
   useEffect(() => {
@@ -45,7 +46,9 @@ const Home = () => {
         <h2>Frontend Developer / YouTuber / JavaScript Expert / Designer</h2>
         <Link to='/contact' className='flat-button'>CONTACT ME</Link>
       </div>
-      <Logo />
+      <div className='my-photo'>
+        <img src={MyPhoto} alt='my photo'/>
+      </div>
     </div>
     <Loader type='pacman'/>
     </>
